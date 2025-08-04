4c0bf36f177a7eee2aa2c90a57d50408a1635547
Mariana Simion dos Santos <mariana.santos@cnsesi.com.br>
2025-08-04 08:59:16 -0300

    :construction:  Construção de telas

--- my-app/src/app/components/CarouselDefault.tsx
+++ my-app/src/app/components/CarouselDefault.tsx
@@ -1,30 +1,67 @@
 "use client";
-import { Carousel } from "@material-tailwind/react";
 import Image from "next/image";
+import { useState } from "react";
 
 export function CarouselDefault() {
+  const initialImages = [
+    "/carro1.jpeg",
+    "/carro4.jpg",
+    "/carro5.jpg"
+  ];
+
+  const [images, setImages] = useState(initialImages);
+  const visibleCount = 3;
+
+  const nextSlide = () => {
+    const [first, ...rest] = images;
+    setImages([...rest, first]);
+  };
+
+  const prevSlide = () => {
+    const last = images[images.length - 1];
+    const rest = images.slice(0, images.length - 1);
+    setImages([last, ...rest]);
+  };
+
   return (
-    <div className="relative bg-red-700 p-4">
+    <div className="relative w-5/6 mx-auto justify-items-center">
+      {/* Botão esquerdo */}
+      <button
+        onClick={prevSlide}
+        className="absolute left-0 top-1/2 transform -translate-y-1/2 z-10 bg-[#EFC0C1] hover:bg-[#dba6a7] text-white font-bold rounded-full w-10 h-10 flex items-center justify-center"
+      >
+        {"<"}
+      </button>
 
-      <p className="black text-2xl">AQUI FICA O CARROSSEL</p>
-      {/* <Carousel className="rounded-xl h-[300px] w-full">
-        <Image
-          src="/carro1.jpeg"
-          alt="Carro 1"
-          width={800}
-          height={300}
-          className="w-full object-cover"
-        />
+      {/* Carrossel */}
+      <div className="overflow-hidden w-full">
+        <div className="flex transition-transform duration-500 ease-in-out" style = {{width: "100%"}}>
+          {images.slice(0, visibleCount).map((src, i) => (
+            <div
+              key={i}
+              className="min-w-[30%] flex justify-center m-4"
+              style={{ height: "300px" }} // altura padrão
+            >
+              <div className="relative w-[400px] h-[300px]">
+                <Image
+                  src={src}
+                  alt={`Imagem ${i + 1}`}
+                  fill
+                  className="rounded-lg object-cover"
+                />
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
 
-        
-        <Image
-          src="/carro2.png"
-          alt="Carro 2"
-          width={800}
-          height={300}
-          className="w-full object-cover"
-        />
-      </Carousel> */}
+      {/* Botão direito */}
+      <button
+        onClick={nextSlide}
+        className="absolute right-0 top-1/2 transform -translate-y-1/2 z-10 bg-[#EFC0C1] hover:bg-[#dba6a7] text-white font-bold rounded-full w-10 h-10 flex items-center justify-center"
+      >
+        {">"}
+      </button>
     </div>
   );
 }
--- my-app/src/app/frentes/page.tsx
+++ my-app/src/app/frentes/page.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import AccordionSection from "../components/AccordionSection";
-import { CarouselDefault } from "../components/CarouselDefault";
+import {CarouselDefault} from "../components/CarouselDefault";
 
 export default function Home() {
   return (
@@ -20,41 +20,44 @@ export default function Home() {
   </div>
 
   <div className="bg-[#F0E6D4] flex flex-col justify-center items-center place-content-center ">
-    <div className="font-bold text-6xl text-[#9C1B2E] text-center w-/6 pt-16 ">CONHEÇA AS FRENTES DE ATUAÇÃO DO MENINAS.COMP</div>
-    <div className="text-black w-5/6  text-3xl font-light p-12">lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum tellus elit sed risus. Maecenas eget condimentum velit, sit amet feugiat lectus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per </div>
+    <div className="font-bold text-6xl text-[#9C1B2E] text-center w-4/6 pt-16 ">CONHEÇA AS FRENTES DE ATUAÇÃO DO MENINAS.COMP</div>
+    <div className="text-black w-6/7  text-3xl font-light p-12 text-justify">Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam molestiae quos nostrum tempore ad harum delectus accusamus id! Accusantium rerum unde accusamus tenetur temporibus, quibusdam aliquam harum praesentium pariatur velit.lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum tellus elit sed risus. Maecenas eget condimentum velit, sit amet feugiat lectus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per </div>
     <div > 
     </div>
 
       <div className="w-5/6">
           <AccordionSection title="COMUNICAÇÃO">
-            <p>Conteúdo sobre a frente de comunicação.</p>
+            <p className="bg-white h-48 rounded-lg text-3xl">Conteúdo sobre a frente de comunicação.</p>
           </AccordionSection>
 
           <AccordionSection title="COMPETIÇÕES">
-            <p>Conteúdo sobre competições.</p>
+            <p className="bg-white h-48 rounded-lg text-3xl">Conteúdo sobre competições.</p>
           </AccordionSection>
 
           <AccordionSection title="COLEÇÕES">
-            <p className="bg-white">Conteúdo sobre coleções.</p>
+            <p className="bg-white h-48 rounded-lg text-3xl">Conteúdo sobre coleções.</p>
           </AccordionSection>
 
           <AccordionSection title="INTELIGÊNCIA ARTIFICIAL">
-            <p>Conteúdo sobre IA.</p>
+            <p className="bg-white h-48 rounded-lg text-3xl">Conteúdo sobre IA.</p>
           </AccordionSection>
 
           <AccordionSection title="SEGURANÇA">
-            <p>Conteúdo sobre segurança.</p>
+            <p className="bg-white h-48 rounded-lg text-3xl">Conteúdo sobre segurança.</p>
           </AccordionSection>
 
           <AccordionSection title="ROBÓTICA">
-            <p>Conteúdo sobre robótica.</p>
+            <p className="bg-white h-48 rounded-lg text-3xl">Conteúdo sobre robótica.</p>
           </AccordionSection>
 
           <AccordionSection title="JOGOS">
-            <p>Conteúdo sobre jogos.</p>
+            <p className="bg-white h-48 rounded-lg text-3xl">Conteúdo sobre jogos.</p>
           </AccordionSection>
         </div>
-    <CarouselDefault/>
+
+    <div className="w-full">    
+      <CarouselDefault/>
+    </div>   
 
   </div>
 </div>
